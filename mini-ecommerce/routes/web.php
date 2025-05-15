09156a56262bde03366d19b4f34c07023d7e1480
MattysLeduc <mattys.leduc@gmail.com>
2025-05-15 18:15:12 -0400

    Made the routes for the different controllers

--- mini-ecommerce/routes/web.php
+++ mini-ecommerce/routes/web.php
@@ -1,11 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Products
+Route::get('/', [ProductController::class,'index'])->name('products.index');
+Route::get('products/{product}', [ProductController::class,'show'])->name('products.show');
+Route::resource('products', ProductController::class)->except(['index','show']);
+
+// Cart
+Route::post('cart/add/{product}',    [CartController::class,'add'])->name('cart.add');
+Route::get('cart',                   [CartController::class,'index'])->name('cart.index');
+Route::patch('cart/update/{product}',[CartController::class,'update'])->name('cart.update');
+Route::delete('cart/remove/{product}',[CartController::class,'remove'])->name('cart.remove');
+
+// Checkout & Orders
+Route::get('checkout', [OrderController::class,'checkoutForm'])->name('checkout.form');
+Route::post('checkout',[OrderController::class,'placeOrder'])->name('checkout.place');
+
+// Admin orders
+Route::middleware(['auth','can:admin'])->group(function(){
+    Route::get('admin/orders',       [OrderController::class,'adminIndex'])->name('admin.orders.index');
+    Route::get('admin/orders/{order}',[OrderController::class,'adminShow'])->name('admin.orders.show');
+});
