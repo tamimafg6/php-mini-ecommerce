a66edfab02ff6f707094ff9556686d360cfeb01a
Tamim Afghanyar <tamim.afghanyar@gmail.com>
2025-05-15 15:47:35 -0400

    added code to migrations

--- mini-ecommerce/database/migrations/2025_05_13_195745_create_products_table.php
+++ mini-ecommerce/database/migrations/2025_05_13_195745_create_products_table.php
@@ -4,24 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+class CreateProductsTable extends Migration {
+    public function up(){
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('category');
+            $table->text('description')->nullable();
+            $table->decimal('price',8,2);
+            $table->string('image')->nullable();
+            $table->unsignedInteger('stock_quantity')->default(0);
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
-};
+    public function down(){ Schema::dropIfExists('products'); }
+}
--- mini-ecommerce/database/migrations/2025_05_13_195748_add_is_admin_to_users.php
+++ mini-ecommerce/database/migrations/2025_05_13_195748_add_is_admin_to_users.php
@@ -4,25 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
+class AddIsAdminToUsers extends Migration {
+    public function up(){
+        Schema::table('users', function (Blueprint $table){
+            $table->boolean('is_admin')->default(false);
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
+    public function down(){
+        Schema::table('users', function (Blueprint $table){
+            $table->dropColumn('is_admin');
         });
     }
-};
+}
