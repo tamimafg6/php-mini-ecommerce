b43eb428b19f4cf7667d8ff2512e2b8263ff81c8
MattysLeduc <mattys.leduc@gmail.com>
2025-05-15 18:18:29 -0400

    Added code to each model

--- mini-ecommerce/app/Models/Order.php
+++ mini-ecommerce/app/Models/Order.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'user_id','name','email','address','payment_type','total_amount'
+    ];
+    public function items(){
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- mini-ecommerce/app/Models/Product.php
+++ mini-ecommerce/app/Models/Product.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'name','category','description','price','image','stock_quantity'
+    ];
 }
